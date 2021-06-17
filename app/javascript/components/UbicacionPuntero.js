ddd3d8c249875ab420b7f55d24448e1f724171b0
davidcrc <david_drcr@hotmail.com>
2021-06-17 17:16:47 -0500

    usin axios

--- app/javascript/components/UbicacionPuntero.js
+++ app/javascript/components/UbicacionPuntero.js
@@ -1,25 +1,37 @@
 import React from "react"
 import PropTypes from "prop-types"
- 
+const axios = require('axios')
+
 class UbicacionPuntero extends React.Component {
   
   constructor(props) {
     super(props);
     this.movimientoPuntero = this.movimientoPuntero.bind(this);
+    this.onclick = this.onclick.bind(this)
     this.state = { x: 0, y: 0 };
     console.log("print props", props);
   }
  
+  
   movimientoPuntero(evento) {
     this.setState({
       x: evento.clientX,
       y: evento.clientY
     });
   }
+ async onclick () {
+   console.log("click aqui")
+   
+    // get data form API
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users?_limit=3')
+    console.log("data", res.data)
+  
+ }
+
  
   render() {
     return (
-      <div style={{ height: '100vh' }} onMouseMove={this.movimientoPuntero}>        
+      <div style={{ height: '100vh' }} onMouseDown={this.onclick} onMouseMove={this.movimientoPuntero}>        
         <p>La posición actual del puntero es: ({this.state.x}, {this.state.y})</p>
       </div>
     );
